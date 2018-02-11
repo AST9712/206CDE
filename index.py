5506f9f306a998e829c9f002efb4d4bad8303397
Tony Hajdini <hajdinim@uni.coventry.com>
2018-02-11 19:46:20 +0200

    making users

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
@@ -7,7 +7,24 @@ app = Flask(__name__)
 def index():
     return 'route'
 
+@app.route('/kwlos')
+def something():
+    if method == 'POST'
+        return 'something'
+    else:
+        return 'else'
+
+    return render_template('templates/index.html')
+
+
+@app.route('/kwlos/moyni')
+
+def something2():
+
+
 
 
 if __name__ == '__main__':
     app.run()
+
+
