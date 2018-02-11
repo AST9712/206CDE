816c3c10b43c69ca67b1d7d51edd60ede9cbd32f
Tony Hajdini <hajdinim@uni.coventry.com>
2018-02-11 19:49:14 +0200

    making user

--- index.py
+++ index.py
@@ -1,25 +1,39 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request
 
 app = Flask(__name__)
 
 # this is the home
 @app.route('/')
 def index():
+    username = request.cookies.get('username')
     return 'route'
 
-@app.route('/kwlos')
-def something():
-    if method == 'POST'
-        return 'something'
+@app.route('/log_in')
+def log_in():
+    ''' log in user '''
+    error = None
+    user_name = request.form['username']
+    user_pass = request.form['password']
+    if request.method == 'POST':
+        if valid_login(user_name,user_pass):
+            return log_the_user_in(['username'])
+
     else:
-        return 'else'
-
-    return render_template('templates/index.html')
+        error = 'The Passwrd or Username is invalid'
 
 
-@app.route('/kwlos/moyni')
 
-def something2():
+    return render_template('templates/index.html',error = error)
+
+
+@app.route('/upload', methods=['POST','GET'])
+def upload_file()
+    if request.method == 'POST': 
+        f = request.files['the files']
+        f.save['/var/upload/file/uploaded_files.txt']
+
+    return render_template('upload_file.html')
+
 
 
 
